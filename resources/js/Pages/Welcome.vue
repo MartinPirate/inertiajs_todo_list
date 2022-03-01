38026b500cfe456048db4ba1ab960cfb406d2783
Martinskiragu <m.kiragu@sweetnexus.co.uk>
2022-03-01 07:49:52 +0300

    render pages and data in inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <h1>Hello, {{ name }}</h1>
+</template>
+
+<script>
+export default {
+    name: "Welcome",
+    props: {
+        name: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
